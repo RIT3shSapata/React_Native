ba8fd5682a01f8b6e877c1bf1ca9ae5b59fafd39
Ritesh Sapata <riteshsapata@gmail.com>
2021-02-10 18:08:25 +0530

    Section 6 Completed

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,10 +1,14 @@
 import { ImageBase } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import ColorScreen from './src/screens/ColorScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
+import CounterScreen from './src/screens/CounterScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import ImageScreen from './src/screens/ImageScreen';
 import ListScreen from './src/screens/ListScreen';
+import SquareScreen from './src/screens/SquareScreen';
+import TextScreen from './src/screens/TextScreen';
 
 const navigator = createStackNavigator(
   {
@@ -12,6 +16,10 @@ const navigator = createStackNavigator(
     Components: ComponentsScreen,
     List: ListScreen,
     Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
+    Text: TextScreen,
   },
   {
     initialRouteName: 'Home',
